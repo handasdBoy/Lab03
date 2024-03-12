d01c0b58fedde945b28fcb6e5f6cd69b1bcd05b1
handasdBoy <161306517+handasdBoy@users.noreply.github.com>
2024-03-12 19:11:29 +0800

    Update JPA02.java

--- JPA02.java
+++ JPA02.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
+public class JPA07{
+    public static void main(String[] args){
+        int z = 27;
+        for(int i=6;i<10;i++){
+            for(int j=2;j<4;j++){
+                if(z==i*j){
+                    System.out.print("count = "+z);
+                    break;
+                }
+            }
+        }
+    }
+}
 
