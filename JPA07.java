3d68f5c412438a0a9a920f422fd37d1609fdbbfe
handasdBoy <161306517+handasdBoy@users.noreply.github.com>
2024-03-12 18:46:36 +0800

    Update JPA07.java

--- JPA07.java
+++ JPA07.java
@@ -1,1 +1,20 @@
-
+import java.util.Scanner;
+public class JPA07{
+    public static void main(String[] args){
+        Scanner a = new Scanner(System.in);
+        while(true){
+            System.out.println("Input:");
+            int b = a.nextInt();
+            if(b==999){
+                break;
+            }
+            int c = a.nextInt();
+            for(int i=b;i>=1;i--){
+                if(b%i==0 && c%i==0){
+                    System.out.println(i);
+                    break;
+                }
+            }
+        }
+    }
+}
